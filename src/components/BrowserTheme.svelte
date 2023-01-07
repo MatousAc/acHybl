9e817c0603932889d5f784abe9a52fd1383e86bc
Ac Hybl <matoush@southern.edu>
2023-01-07 16:02:50 -0500

    test new browser theme script

--- src/components/BrowserTheme.svelte
+++ src/components/BrowserTheme.svelte
@@ -1,32 +1,41 @@
 <script lang="ts">
+	import { onMount } from "svelte";
   import { theme } from "../ts/stores";
+	import Bar from "./Bar.svelte";
 
-	let color: string;
-  theme.subscribe(val => {
-    if (val === "dark") {
-      color = "#240090"
-    } else if (val === "light") {
-      color = "#494949"
-    } else {
-      color = "var(--address-bar-color)"
-    }
+  let meta: HTMLMetaElement;
+  onMount(() => {
+    theme.subscribe(val => {
+      if (val === "dark") {
+        meta.setAttribute("content", "#240090")
+      } else if (val === "light") {
+        meta.setAttribute("content", "#494949")
+      } else {
+        meta.setAttribute("content", 
+          getComputedStyle(meta).getPropertyValue("--clr"))
+      }
+    })
   })
 </script>
 
 <svelte:head>
-  <meta name="theme-color" content={color} >
+  <meta bind:this={meta} name="theme-color">
 </svelte:head>
 
 <style>
+meta {
+  --clr: "#240090";
+}
+
 @media (prefers-color-scheme: light) {
-  :root {
-    --address-bar-color: "#494949";
+  meta {
+    --clr: "#494949";
   }
 }
 
 @media (prefers-color-scheme: dark) {
-  :root {
-    --address-bar-color: "#240090";
+  meta {
+    --clr: "#240090";
   }
 }
 </style>
