2057aa2ace03cec376193232fbca8673b952361d
Ac Hybl <matoush@southern.edu>
2023-01-07 16:14:57 -0500

    browser should now change with system theme

--- src/components/BrowserTheme.svelte
+++ src/components/BrowserTheme.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { onMount } from "svelte";
   import { theme } from "../ts/stores";
-	import Bar from "./Bar.svelte";
 
   let meta: HTMLMetaElement;
+
+  let setSystemTheme = () => {
+    meta.setAttribute("content", 
+      getComputedStyle(meta).getPropertyValue("--clr"))
+  }
+  
   onMount(() => {
     theme.subscribe(val => {
       if (val === "dark") {
@@ -11,8 +16,9 @@
       } else if (val === "light") {
         meta.setAttribute("content", "#494949")
       } else {
-        meta.setAttribute("content", 
-          getComputedStyle(meta).getPropertyValue("--clr"))
+        setSystemTheme()
+        window.matchMedia('(prefers-color-scheme: dark)')
+          .addEventListener('change', setSystemTheme)
       }
     })
   })
@@ -24,18 +30,18 @@
 
 <style>
 meta {
-  --clr: "#240090";
+  --clr: #240090;
 }
 
 @media (prefers-color-scheme: light) {
   meta {
-    --clr: "#494949";
+    --clr: #494949;
   }
 }
 
 @media (prefers-color-scheme: dark) {
   meta {
-    --clr: "#240090";
+    --clr: #240090;
   }
 }
 </style>
