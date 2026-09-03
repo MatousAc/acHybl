414857d574118929df01e1fea86d7e23b49a1ecc
MatousAc <56884762+MatousAc@users.noreply.github.com>
2026-09-03 00:10:54 -0400

    add lowestAsk

--- src/assets/cardObject.js
+++ src/assets/cardObject.js
@@ -4,12 +4,12 @@
 // Occasionally adds styling to a card
 
 const addPrimeStyles = () => {
-	if (document.getElementById('prime-card-styles')) return;
+    if (document.getElementById('prime-card-styles')) return;
 
-	const style = document.createElement('style');
-	style.id = 'prime-card-styles';
+    const style = document.createElement('style');
+    style.id = 'prime-card-styles';
 
-	style.textContent = `
+    style.textContent = `
         .prime-card-corner {
             position: absolute;
             top: 0;
@@ -50,175 +50,177 @@ const addPrimeStyles = () => {
         }
     `;
 
-	document.head.appendChild(style);
-};
+    document.head.appendChild(style);
+}
 
 class Card {
-	constructor(container) {
-		this.container = container;
-		// basic information
-		this.season = this.getSeason();
-		this.category = this.getCategory();
-		this.region = this.getRegion();
-		this.id = this.getId();
-		this.info = this.getInfo();
-		console.log(this.info);
+    constructor(container) {
+        this.container = container
+        // basic information
+        this.season = this.getSeason()
+        this.category = this.getCategory()
+        this.region = this.getRegion()
+        this.id = this.getId()
+        this.info = this.getInfo()
+        console.log(this.info)
 
-		if (this.isPrime()) {
-			this.markAsPrime();
-		}
-	}
+        if (this.isPrime()) {
+            this.markAsPrime()
+        }
+    }
 
-	getSeason() {
-		let info = this.container.querySelector('[class^=deckcard][data-season]');
-		if (!info) return -1;
-		return parseInt(info.getAttribute('data-season'));
-	}
+    getSeason() {
+        let info = this.container.querySelector('[class^=deckcard][data-season]')
+        if (!info) return -1
+        return parseInt(info.getAttribute('data-season'))
+    }
 
-	getCategory() {
-		let category = this.container.children[0].children[0].classList[1];
-		category = category.replace('deckcard-category-', '');
-		return category;
-	}
+    getCategory() {
+        let category = this.container.children[0].children[0].classList[1]
+        category = category.replace('deckcard-category-', '')
+        return category
+    }
 
-	getRegion() {
-		return this.container.getElementsByClassName('deckcard-region')[0];
-	}
+    getRegion() {
+        return this.container.getElementsByClassName('deckcard-region')[0]
+    }
 
-	getId() {
-		let infoBtn = this.container.querySelector('.deckcard-info');
-		let anchor = infoBtn.querySelector('a');
-		console.log('getting id');
-		return anchor.href.match(/(?<=\/card=)\d+/)?.[0];
-	}
+    getId() {
+        let infoBtn = this.container.querySelector('.deckcard-info')
+        let anchor = infoBtn.querySelector('a')
+        console.log("getting id")
+        return anchor.href.match(/(?<=\/card=)\d+/)?.[0]
+    }
 
-	getInfo() {
-		let info = {};
-		let isCardPage = window.location.href.includes('card=');
-		let infoTable = document.getElementById('rces-infotable');
-		if (!isCardPage || !infoTable) {
-			//setTimeout(() => this.info = this.getInfo(), 2000)
-			return info;
-		}
+    getInfo() {
+        let info = {}
+        let isCardPage = window.location.href.includes('card=')
+        let infoTable = document.getElementById('rces-infotable')
+        if (!isCardPage || !infoTable) {
+            //setTimeout(() => this.info = this.getInfo(), 2000)
+            return info
+        }
 
-		// get info about card
-		let rcesTableRows = [...infoTable.children[0].children];
-		rcesTableRows.forEach((row) => {
-			let data = [...row.children];
-			info[data[0].innerHTML.toLowerCase()] = parseFloat(data[1].innerHTML);
-		});
+        // get info about card
+        let rcesTableRows = [...infoTable.children[0].children]
+        rcesTableRows.forEach(row => {
+            let data = [...row.children]
+            info[data[0].innerHTML.toLowerCase()] = parseFloat(data[1].innerHTML)
+        })
 
-		info.season = this.season;
-		info.category = this.category;
+        info.season = this.season
+        info.category = this.category
 
-		// get bid info
-		let unmatchedBids = [...document.getElementsByClassName('cardauctionunmatchedrow-bid')];
-		let matchedBids = [...document.getElementsByClassName('cardauction-highestmatchedoffer')];
-		let bids = unmatchedBids.concat(matchedBids);
-		if (bids.length > 0) {
-			info.highestBid = parseFloat(
-				bids[bids.length - 1].getElementsByClassName('cardprice')[0].innerHTML
-			);
-		} else {
-			info.highestBid = 0;
-		}
+        // get bid info
+        let unmatchedBids = [...document.getElementsByClassName('cardauctionunmatchedrow-bid')]
+        let matchedBids = [...document.getElementsByClassName('cardauction-highestmatchedoffer')]
+        let bids = unmatchedBids.concat(matchedBids)
+        if (bids.length > 0) {
+            info.highestBid = parseFloat(bids[bids.length - 1].getElementsByClassName('cardprice')[0].innerHTML)
+        } else {
+            info.highestBid = 0
+        }
+        let asks = [...document.getElementsByClassName('cardauctionunmatchedrow-ask')]
+        if (asks.length > 0) {
+            info.lowestAsk = parseFloat(asks[0].getElementsByClassName('cardprice')[0].innerHTML)
+        }
 
-		info.mv = info['market value (estimated)'];
-		info.recentSellPrice = info['most recent sell price'];
-		info.highestSellPrice = info['highest sell price'];
-		info.jv = info['junk price'];
-		return info;
-	}
+        info.mv = info['market value (estimated)']
+        info.recentSellPrice = info['most recent sell price']
+        info.highestSellPrice = info['highest sell price']
+        info.jv = info['junk price']
+        return info
+    }
 
-	iOwn() {
-		let minorInfo = document.getElementsByClassName('minorinfo');
-		return !(minorInfo.length == 0);
-	}
+    iOwn() {
+        let minorInfo = document.getElementsByClassName('minorinfo')
+        return !(minorInfo.length == 0)
+    }
 
-	isExNation() {
-		if (!this.region) return false;
-		return this.region.innerHTML.includes('Ex-Nation');
-	}
+    isExNation() {
+        if (!this.region) return false
+        return this.region.innerHTML.includes('Ex-Nation')
+    }
 
-	isPrime() {
-		let n = this.id;
-		if (n <= 1) return false;
-		if (n <= 3) return true;
-		if (n % 2 === 0 || n % 3 === 0) return false;
+    isPrime() {
+        let n = this.id
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 === 0 || n % 3 === 0) return false;
 
-		// Only check up to the square root of n
-		const limit = Math.sqrt(n);
-		for (let i = 5; i <= limit; i += 6) {
-			if (n % i === 0 || n % (i + 2) === 0) return false;
-		}
-		return true;
-	}
+        // Only check up to the square root of n
+        const limit = Math.sqrt(n);
+        for (let i = 5; i <= limit; i += 6) {
+            if (n % i === 0 || n % (i + 2) === 0) return false;
+        }
+        return true;
+    }
 
-	isLeg() {
-		return this.category == 'legendary';
-	}
+    isLeg() {
+        return this.category == 'legendary'
+    }
 
-	junk() {
-		let junkBtn = this.container.getElementsByClassName('deckcard-junk-button')[0];
-		junkBtn.click();
-	}
+    junk() {
+        let junkBtn = this.container.getElementsByClassName('deckcard-junk-button')[0]
+        junkBtn.click()
+    }
 
-	giftPage() {
-		console.log(`gifting card`);
-		let giftBtn = this.container.querySelector('.deckcard-info-cardbuttons').children[1];
-		window.open(giftBtn.href + '?tamperAction=gift', '_self');
-	}
+    giftPage() {
+        console.log(`gifting card`)
+        let giftBtn = this.container.querySelector('.deckcard-info-cardbuttons').children[1]
+        window.open(giftBtn.href + '?tamperAction=gift', '_self')
+    }
 
-	infoPage() {
-		let infoBtn = this.container.getElementsByClassName('deckcard-info-cardlink')[0];
-		let href = infoBtn.children[0].href;
-		window.open(href, '_blank');
-	}
+    infoPage() {
+        let infoBtn = this.container.getElementsByClassName('deckcard-info-cardlink')[0]
+        let href = infoBtn.children[0].href
+        window.open(href,'_blank')
+    }
 
-	manage(tamperAction) {
-		let isCardPage = window.location.href.includes('card=');
-		if (!isCardPage) return;
-		let manageLink = document.querySelector('a.manage_collections_button');
-		if (!manageLink) return;
-		manageLink.href += `&tamperAction=${tamperAction}`;
-		manageLink.click();
-	}
+    manage(tamperAction) {
+        let isCardPage = window.location.href.includes('card=')
+        if (!isCardPage) return
+        let manageLink = document.querySelector('a.manage_collections_button')
+        if (!manageLink) return
+        manageLink.href += `&tamperAction=${tamperAction}`
+        manageLink.click()
+    }
 
-	markAsPrime() {
-		let flag;
+    markAsPrime() {
+        let flag;
 
-		switch (this.season) {
-			case 1:
-			case 2:
-				flag = this.container.querySelector('.deckcard-flag');
-				break;
+        switch (this.season) {
+            case 1:
+            case 2:
+                flag = this.container.querySelector('.deckcard-flag');
+                break;
 
-			case 3:
-				flag = this.container.querySelector('.s3-flag');
-				break;
+            case 3:
+                flag = this.container.querySelector('.s3-flag');
+                break;
 
-			case 4:
-				flag = this.container.querySelector('main.flag');
-				break;
+            case 4:
+                flag = this.container.querySelector('main.flag');
+                break;
 
-			default:
-				console.warn('Unknown card season:', this.season);
-				return;
-		}
+            default:
+                console.warn('Unknown card season:', this.season);
+                return;
+        }
 
-		if (!flag) return;
+        if (!flag) return;
 
-		if (flag.querySelector('.prime-card-corner')) {
-			return;
-		}
+        if (flag.querySelector('.prime-card-corner')) {
+            return;
+        }
 
-		if (getComputedStyle(flag).position === 'static') {
-			flag.style.position = 'relative';
-		}
+        if (getComputedStyle(flag).position === 'static') {
+            flag.style.position = 'relative';
+        }
 
-		const corner = document.createElement('span');
-		corner.className = 'prime-card-corner';
+        const corner = document.createElement('span');
+        corner.className = 'prime-card-corner';
 
-		flag.appendChild(corner);
-	}
+        flag.appendChild(corner);
+    }
 }
